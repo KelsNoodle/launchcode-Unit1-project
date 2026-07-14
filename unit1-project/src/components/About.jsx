8d8172cbe3d4971435b026712863c5ef477da98b
Kelsea W <kelsea.wessel@gmail.com>
2026-07-14 13:10:55 -0500

    completed styling and site themeing

---
+++ unit1-project/src/components/About.jsx
@@ -0,0 +1,11 @@
+export default function AboutMe() {
+    return(
+        <div id="mecard">
+            <img src="author.png" id="drawncards" alt="Image of the author of the website" height="400"></img>
+            <hr></hr>
+            <p>Hello and welcome to my project site!</p>
+            <p>This site is both a project to show off my skills and to also become a free resource to learn about birds!</p>
+            <p>If you are like me, the joy of pulling trading cards can be both exciting and </p>
+        </div>
+    )
+}
