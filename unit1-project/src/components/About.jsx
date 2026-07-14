f0914f02969b42c2859208fcb942f23d6e234db7
Kelsea W <kelsea.wessel@gmail.com>
2026-07-14 14:31:17 -0500

    completed about me component

--- unit1-project/src/components/About.jsx
+++ unit1-project/src/components/About.jsx
@@ -3,9 +3,27 @@ export default function AboutMe() {
         <div id="mecard">
             <img src="author.png" id="drawncards" alt="Image of the author of the website" height="400"></img>
             <hr></hr>
-            <p>Hello and welcome to my project site!</p>
-            <p>This site is both a project to show off my skills and to also become a free resource to learn about birds!</p>
-            <p>If you are like me, the joy of pulling trading cards can be both exciting and </p>
+            <div id="aboutmetext">
+                <p>Hello and welcome to my project site!</p>
+                <p>This site is both a project to show off my skills and to also become a free resource to learn about birds!
+                    I created this site as part of my software development course and have made this with the use of:
+                </p>
+                <p><ul>
+                        <li>React</li>
+                        <li>Javascript</li>
+                        <li>Vite</li>
+                        <li>VSCode</li>
+                        <li>GIT</li>
+                        <li>HTML/CSS</li>
+                    </ul></p>
+                <p>If you are like me, the joy of pulling trading cards can be both fun and exciting!
+                The purpose of this site is to both educate users on birds while also enjoying the 
+                thrill of pulling a mystery set of trading cards. 
+                More cards will be added over time, so please come visit regularly for new surprises! 
+                </p>
+                <p>I hope you can enjoy the cards I have put together and hope that you can share this with your peers,
+                    loved ones, or students!</p>
+            </div>
         </div>
     )
 }
