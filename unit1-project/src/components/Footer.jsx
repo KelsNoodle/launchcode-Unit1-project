e6f0ccd2e7a09ab801854ddcc7be5620993edc82
Kelsea W <kelsea.wessel@gmail.com>
2026-07-17 11:10:02 -0500

    added footer component

---
+++ unit1-project/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+        Created by Kelsea Wessel - <a href="https://github.com/KelsNoodle">My Github</a>
+        </footer>
+    )
+}
